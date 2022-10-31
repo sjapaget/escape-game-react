15989abdcfaf7e19beae52f1bf02c88b7d814b72
Samuel Paget <sjapaget@gmail.com>
2022-10-31 12:24:40 +0100

    Add state for scene with conditional rendering for scene components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-import Start from "./components/Start"
-import Scene1 from "./components/Scene1"
+import {useState} from 'react';
+import Start from "./components/Start";
+import Scene1 from "./components/Scene1";
 
 
 function App() {
+
+  const [scene, setScene] = useState(0)
+
   return (
     <div className="flex flex-row justify-center h-screen bg-orange-50 p-4 w-screen font-mono">
-      {/* <Start /> */}
-      <Scene1 />
+      {scene === 0 && <Start />}
+      {scene === 1 && <Scene1 />}
     </div>
   )
 }
