34f52a996ff3b56034a87d7259840c9f4ec99ffa
Samuel Paget <sjapaget@gmail.com>
2022-10-31 12:43:56 +0100

    remoe console log and whitespace

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,10 @@ function App() {
 
   const [scene, setScene] = useState(0)
 
-  console.log(scene)
-
   const nextScene = () => {
     setScene(prevScene => prevScene + 1)
   }
-  
+
   return (
     <div className="flex flex-row justify-center h-screen bg-orange-50 p-4 w-screen font-mono">
       {scene === 0 && <Start sceneChange={nextScene} />}
