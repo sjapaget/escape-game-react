43ef2b85e2f22702406bded31f65d4aa669edee7
Samuel Paget <sjapaget@gmail.com>
2022-10-31 12:37:19 +0100

    Add scene change function

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,15 @@ function App() {
 
   const [scene, setScene] = useState(0)
 
+  console.log(scene)
+
+  const nextScene = () => {
+    setScene(prevScene => prevScene + 1)
+  }
+  
   return (
     <div className="flex flex-row justify-center h-screen bg-orange-50 p-4 w-screen font-mono">
-      {scene === 0 && <Start />}
+      {scene === 0 && <Start sceneChange={nextScene} />}
       {scene === 1 && <Scene1 />}
     </div>
   )
--- src/components/Start.jsx
+++ src/components/Start.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 
 import Button from "./Button"
 
-const Start = () => {
+const Start = props => {
   return (
     <div className='flex flex-col justify-between p-12 bg-slate-100 rounded-lg'>
 
       <h1 className='text-8xl my-4'>Citizen Saviour</h1>
 
-      <Button text={"Start"} />
+      <Button
+        text={"Start"}
+        nextScene={props.sceneChange}
+       />
     </div>
   )
 }
