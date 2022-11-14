f4f69fada0e7b6d0a23e45b36f4ed3eb087659d8
Samuel Paget <sjapaget@gmail.com>
2022-11-14 10:54:23 +0100

    Update logo image src and path

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import logoUrl from '/home/sam/code/sjapaget/react/projects/escape-game-react/src/assets/Esc-Logo-8bit-colour.png'
 
 const Nav = (props) => {
   const { scene } = props;
   return (
     <div className='flex flex-row justify-between items-center bg-orange-500 text-neutral-100'>
-      <img className='w-12 m-1' src="/src/assets/Esc-Logo-8bit.png" alt="logo" />
+      <img className='w-12 m-1' src={logoUrl} alt="logo" />
       <ul className='flex flex-row'>
         <li className='px-4'>{scene}/6</li>
         <li className='px-4'>Time Left: 60:00</li>
